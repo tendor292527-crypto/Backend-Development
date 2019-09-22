5525c519f19ef57bfba38d95a2cdeac92ebcb5b3
BrianKoss951001 <tendor292527@gmail.com>
2019-09-21 21:54:13 -0400

    commit images

---
+++ dashboard/acme/home.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home | Acme</title>
+    <style> 
+    body{
+        background-image:url(/dashboard/acme/images/background.jpg);
+        background-size: cover; 
+        background-attachment: fixed;
+    }
+    .content{
+        background: white;
+        width:70%;
+        padding:10px;
+        margin:50px auto; 
+    }
+    p{
+        font-size:20px;
+        color:black;
+    }
+    </style>
+</head>
+<body>
+    <div class="content">
+        <header>
+            <img src="" alt="Acme Logo">
+            <img src="" alt="My Account Folder">
+        </header>
+        <main>
+            <h2>Welcome To Acme</h2>
+            <div>
+            
+                <img src="" alt="">
+                <aside>
+                    <h2>Acme Rocket</h2>
+                    <p>Quick lighting fuse </p>
+                    <p>NTHSA approved seat belts MO</p>
+                    <p>Mobile launch stand included</p>
+                    
+                    <a href="#"><img href="#" src="" alt="I want it now button!"></a>
+                </aside>
+            </div>
+        </main>
+        
+    
+    </div>
+    
+</body>
+</html>
