939e72027adf41e222e0f00feb1c9af631e504c2
BrianKoss951001 <tendor292527@gmail.com>
2019-09-24 23:56:35 -0400

    fixing some errors

--- dashboard/acme/modules/footer.php
+++ dashboard/acme/modules/footer.php
@@ -4,4 +4,4 @@
             All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be
             removed.<br>
             Last updated: September 24, 2019
-        </footer>
+</footer>
