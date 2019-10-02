1a97b33504b74340beff4841b45c50ff37c8ab80
BrianKoss951001 <tendor292527@gmail.com>
2019-10-01 22:39:02 -0400

    creating error file and acme function

--- dashboard/acme/library/connections.php
+++ dashboard/acme/library/connections.php
@@ -2,17 +2,20 @@
 /*
 Database Connections 
 */
-$server = "localhost";
-$database = "acme";
-$user = "iClient";
-$password = "YOGEfETechF4TiNQ";
-$dsn = 'mysql:host='.$server.';dbname='.$database;
-$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+function acmeConnect(){
+    $server = "localhost";
+    $database = "acme";
+    $user = "iClient";
+    $password = "YOGEfETechF4TiNQ";
+    $dsn = 'mysql:host='.$server.';dbname='.$database;
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
-try{
-    $acmeLink = new PDO($dsn, $user, $password, $options);
-    echo '$acmeLink worked succesfully<br>'; 
-}catch(PDOException $exc){
-    echo $exc->getMessage();
+    try{
+        $acmeLink = new PDO($dsn, $user, $password, $options);
+        return $acmeLink;
+    }catch(PDOException $exc){
+        header('location: /dashboard/acme/view/500.php');
+        exit;
+    }
 }
 ?>
---
+++ dashboard/acme/view/500.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/dashboard/acme/css/normalize.css">
+    <link rel="stylesheet" href="/dashboard/acme/css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Be+Vietnam&display=swap" rel="stylesheet">
+    <title> Template | Acme Inc.</title>
+</head>
+
+<body>
+    <div class="content">
+        <header>
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/dashboard/acme/modules/header.php'; ?>
+        </header>
+
+        <nav id="navigation" class="">
+            <?php include $_SERVER['DOCUMENT_ROOT'].'/dashboard/acme/modules/nav.php'; ?>
+        </nav>
+
+        <main id="main">
+            <h2>Server Error!</h2>
+            <p>Sorry, the server experienced a problem</p> 
+        </main>
+
+        <footer id="footer">
+            <?php include $_SERVER['DOCUMENT_ROOT']."/dashboard/acme//modules/footer.php"; ?>
+            <p id="lastupdated">Last Updated: <?php echo date('j F, Y', getlastmod()) ?></p>
+        </footer>
+    </div>
+</body>
+
+</html>
