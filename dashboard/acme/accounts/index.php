7fb2e11586def1b0d24b2dd196645a0f92a42614
BrianKoss951001 <tendor292527@gmail.com>
2019-10-29 23:21:18 -0400

    hashing password in the registration

--- dashboard/acme/accounts/index.php
+++ dashboard/acme/accounts/index.php
@@ -36,41 +36,42 @@ if($action == NULL){
     $action = filter_input(INPUT_GET, 'action');
 }
 switch($action){
-        case 'login':
-            include '../view/login.php';
-            break;
-        case 'register':
-                // echo 'You are in the register case statement.';
-                // exit;
-    
-            // Filter, sanitize and store the data
-            $clientFirstname = filter_input(INPUT_POST, 'clientFirstname', FILTER_SANITIZE_STRING);
-            $clientLastname = filter_input(INPUT_POST, 'clientLastname', FILTER_SANITIZE_STRING);
-            $clientEmail = filter_input(INPUT_POST, 'clientEmail', FILTER_SANITIZE_EMAIL);
-            $clientPassword = filter_input(INPUT_POST, 'clientPassword', FILTER_SANITIZE_STRING);
-            $clientEmail = checkEmail($clientEmail);
-            $clientPassword = checkPassword($clientPassword);
-            // Check for missing data
-            if(empty($clientFirstname) || empty($clientLastname) || empty($clientEmail) || empty($clientPassword)){
-            $message = '<p>Please provide information for all empty form fields.</p>';
-            include '../view/registration.php';
-            exit; 
-        }
-
-        // Send the data to the model
-            $regOutcome = regClient($clientFirstname, $clientLastname, $clientEmail, $clientPassword);
-
-            // Check and report the result
-            if($regOutcome === 1){
-            $message = "<p>Thanks for registering $clientFirstname. Please use your email and password to login.</p>";
-            include '../view/login.php';
-            exit;
-        } else {
-            $message = "<p>Sorry $clientFirstname, but the registration failed. Please try again.</p>";
-            include '../view/registration.php';
-            exit;
-        }
+    case 'login':
+        include '../view/login.php';
         break;
+    case 'register':
+            // echo 'You are in the register case statement.';
+            // exit;
+
+        // Filter, sanitize and store the data
+        $clientFirstname = filter_input(INPUT_POST, 'clientFirstname', FILTER_SANITIZE_STRING);
+        $clientLastname = filter_input(INPUT_POST, 'clientLastname', FILTER_SANITIZE_STRING);
+        $clientEmail = filter_input(INPUT_POST, 'clientEmail', FILTER_SANITIZE_EMAIL);
+        $clientPassword = filter_input(INPUT_POST, 'clientPassword', FILTER_SANITIZE_STRING);
+        $clientEmail = checkEmail($clientEmail);
+        $clientPassword = checkPassword($clientPassword);
+        // Check for missing data
+        if(empty($clientFirstname) || empty($clientLastname) || empty($clientEmail) || empty($clientPassword)){
+        $message = '<p>Please provide information for all empty form fields.</p>';
+        include '../view/registration.php';
+        exit; 
+    }
+    //Protecting the password using a password_hash method 
+    $hashedPassword = password_hash($clientPassword, PASSWORD_DEFAULT);
+    // Send the data to the model
+    $regOutcome = regClient($clientFirstname, $clientLastname, $clientEmail, $clientPassword);
+
+    // Check and report the result
+    if($regOutcome === 1){
+    $message = "<p>Thanks for registering $clientFirstname. Please use your email and password to login.</p>";
+    include '../view/login.php';
+    exit;
+        } else {
+    $message = "<p>Sorry $clientFirstname, but the registration failed. Please try again.</p>";
+    include '../view/registration.php';
+    exit;
+        }
+    break;
 
 }
 ?>
